cbb293e9a2d13bce4e5ad1bf5e6987fbe6518b67
damien sauvalle <damiensauvalle@gmail.com>
2018-02-13 11:41:45 +0100

    feat : add IChatRom Interface

--- src/main/java/Interface/IChatRoom.java
+++ src/main/java/Interface/IChatRoom.java
@@ -1,4 +1,14 @@
 package Interface;
 
-public interface IChatRoom {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IChatRoom extends Remote {
+
+    public void register(IClient client);
+
+    public void send(IMessage message) throws RemoteException;
+
+    public void disconnect(IClient client);
+
 }
