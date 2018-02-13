7d410761443dcc0b73f983f0bdd1ec1178c27d7b
damien sauvalle <damiensauvalle@gmail.com>
2018-02-13 11:38:28 +0100

    feat : add ICLient Interface

--- src/main/java/Interface/IClient.java
+++ src/main/java/Interface/IClient.java
@@ -1,4 +1,21 @@
 package Interface;
 
-public interface IClient {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IClient extends Remote {
+
+    public void setPseudo(String pseudo);
+
+    public String getPseudo();
+
+    public void setPassword(String password);
+
+    public String getPassword();
+
+    public boolean isConnected();
+
+    public void setConnected(boolean connected);
+
+    public void receive(IMessage message) throws RemoteException;
 }
